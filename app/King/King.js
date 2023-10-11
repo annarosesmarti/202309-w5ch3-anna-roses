dec6e424a65840a862a666522203c00f160fe6ac
Anna Roses <anna.roses.marti@gmail.com>
2023-10-12 00:07:58 +0200

    Add constructor to King class

--- app/King/King.js
+++ app/King/King.js
@@ -3,6 +3,11 @@ import Character from "../Character/Character.js";
 class King extends Character {
   yearsOfReign;
 
+  constructor(characterName, yearsInThrone) {
+    super(characterName);
+    this.yearsOfReign = yearsInThrone;
+  }
+
   communication() {
     return `${super.communication()}. You are all gonna die.`;
   }
